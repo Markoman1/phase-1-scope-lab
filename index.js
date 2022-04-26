4284671abdfee65adf6bd7c7ed4533aaa5ffab1d
Mark <mgalex12@icloud.com>
2022-04-26 16:21:09 -0400

    commit tree fe285d58eff625d673e6c76e5aa88f2109347384

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+ var customerName = "bob" 
+ function upperCaseCustomerName() {  
+     customerName = customerName.toUpperCase()
+ console.log(customerName.toUpperCase())
+ }
+
+
+ function setBestCustomer() {
+   bestCustomer = "not bob"
+ }
+
+
+ function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+ }
+
+const leastFavoriteCustomer = "Moe"
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Curly"
+}
